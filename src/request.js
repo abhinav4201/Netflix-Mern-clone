ade3ecd7de53fb7712a741f768421480eab4e3db
Abhinav Anand <75775527+abhinav4201@users.noreply.github.com>
2022-11-18 02:24:30 +0530

    commit by uploading files

---
+++ src/request.js
@@ -0,0 +1,14 @@
+const apiKey = "4b57053dbf7659cb06f184ae5772756c";
+
+const requests = {
+    fetchTrending : `/trending/all/week?api_key=${apiKey}&language=en-US`,
+    fetchNetflixOriginals : `/discover/tv?api_key=${apiKey}&with_networks=213`,
+    fetchTopRated : `/movie/top_rated?api_key=${apiKey}&language=en-US`,
+    fetchActionMovies : `/discover/movie?api_key=${apiKey}&with_genres=28`,
+    fetchComedyMovies : `/discover/movie?api_key=${apiKey}&with_genres=35`,
+    fetchHorrorMovies : `/discover/movie?api_key=${apiKey}&with_genres=27`,
+    fetchRomanceMovies : `/discover/movie?api_key=${apiKey}&with_genres=10749`,
+    fetchDocumentaries : `/discover/movie?api_key=${apiKey}&with_genres=99`,
+}
+
+export default requests;
